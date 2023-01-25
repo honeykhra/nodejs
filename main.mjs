ef25a54ff6f7838095e6962bcda455256acc30c5
honeykhra <honeykhra@gmail.com>
2023-01-25 18:20:42 +0530

    esm added

---
+++ main.mjs
@@ -0,0 +1,4 @@
+import add from "./math-esm.mjs";
+
+console.log("Es Modules");
+console.log(add(2, 3));
---
+++ objectRefrence.js
@@ -0,0 +1,11 @@
+const obj1 = {
+  name: "Divreet Kaur",
+};
+
+let obj2 = obj1;
+obj2 = {
+    name: "Sift Singh",
+}
+
+console.log(obj1);
+console.log(obj2);
